10b36ea59e462bd997213b10dd78f261d954fc8e
rohanrai123 <108138780+rohanrai123@users.noreply.github.com>
2024-08-01 12:05:42 +0000

    feat: initiated the project

--- Proxy.sh
+++ Proxy.sh
@@ -1,48 +1,45 @@
 #!/bin/sh
 
-# Download and execute initial setup scripts
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
 sleep 4
 
-# Update and upgrade the system
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update && sudo apt get upgrade -y
 clear
 
-# Install Hardhat and dependencies
-echo "Installing Hardhat and dotenv..."
+echo "Installing dependencies..."
 npm install --save-dev hardhat
 npm install dotenv
 npm install @swisstronik/utils
+npm install @openzeppelin/hardhat-upgrades
+npm install @openzeppelin/contracts
+npm install @nomicfoundation/hardhat-toolbox
 echo "Installation completed."
 
-# Create a Hardhat project
 echo "Creating a Hardhat project..."
 npx hardhat
 
-# Remove default Lock.sol contract
 rm -f contracts/Lock.sol
 echo "Lock.sol removed."
 
-# Install Hardhat toolbox
 echo "Hardhat project created."
+
 echo "Installing Hardhat toolbox..."
 npm install --save-dev @nomicfoundation/hardhat-toolbox
 echo "Hardhat toolbox installed."
 
-# Create .env file for private key
 echo "Creating .env file..."
 read -p "Enter your private key: " PRIVATE_KEY
 echo "PRIVATE_KEY=$PRIVATE_KEY" > .env
 echo ".env file created."
 
-# Configure Hardhat
 echo "Configuring Hardhat..."
 cat <<EOL > hardhat.config.js
 require("@nomicfoundation/hardhat-toolbox");
+require('@openzeppelin/hardhat-upgrades');
 require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
@@ -53,7 +50,6 @@ module.exports = {
 EOL
 echo "Hardhat configuration completed."
 
-# Create Hello_swtr.sol contract
 echo "Creating Hello_swtr.sol contract..."
 mkdir -p contracts
 cat <<EOL > contracts/Hello_swtr.sol
@@ -63,7 +59,7 @@ pragma solidity ^0.8.19;
 contract Swisstronik {
     string private message;
 
-    constructor(string memory _message) payable {
+    function initialize(string memory _message) public {
         message = _message;
     }
 
@@ -78,40 +74,56 @@ contract Swisstronik {
 EOL
 echo "Hello_swtr.sol contract created."
 
-# Compile the contract
 echo "Compiling the contract..."
 npx hardhat compile
 echo "Contract compiled."
 
-# Create deploy.js script
 echo "Creating deploy.js script..."
 mkdir -p scripts
 cat <<EOL > scripts/deploy.js
-const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik from feature_earning!!"]);
-  await contract.waitForDeployment();
-  console.log(\`Swisstronik contract deployed to \${contract.target}\`);
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Swisstronik = await ethers.getContractFactory('Swisstronik');
+  const swisstronik = await Swisstronik.deploy();
+  await swisstronik.waitForDeployment(); 
+  console.log('Non-proxy Swisstronik deployed to:', swisstronik.target);
+  fs.writeFileSync("contract.txt", swisstronik.target);
+
+  console.log(\`Deployment transaction hash: https://explorer-evm.testnet.swisstronik.com/address/\${swisstronik.target}\`);
+
+  console.log('');
+  
+  const upgradedSwisstronik = await upgrades.deployProxy(Swisstronik, ['Hello Swisstronik from Happy Cuan Airdrop!!'], { kind: 'transparent' });
+  await upgradedSwisstronik.waitForDeployment(); 
+  console.log('Proxy Swisstronik deployed to:', upgradedSwisstronik.target);
+  fs.writeFileSync("proxiedContract.txt", upgradedSwisstronik.target);
+
+  console.log(\`Deployment transaction hash: https://explorer-evm.testnet.swisstronik.com/address/\${upgradedSwisstronik.target}\`);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 EOL
 echo "deploy.js script created."
 
-# Deploy the contract
 echo "Deploying the contract..."
 npx hardhat run scripts/deploy.js --network swisstronik
 echo "Contract deployed."
 
-# Create setMessage.js script
 echo "Creating setMessage.js script..."
 cat <<EOL > scripts/setMessage.js
 const hre = require("hardhat");
 const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const fs = require("fs");
 
 const sendShieldedTransaction = async (signer, destination, data, value) => {
   const rpclink = hre.network.config.url;
@@ -125,12 +137,12 @@ const sendShieldedTransaction = async (signer, destination, data, value) => {
 };
 
 async function main() {
-  const contractAddress = "0xf84Df872D385997aBc28E3f07A2E3cd707c9698a";
+  const contractAddress = fs.readFileSync("proxiedContract.txt", "utf8").trim();
   const [signer] = await hre.ethers.getSigners();
   const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
   const contract = contractFactory.attach(contractAddress);
   const functionName = "setMessage";
-  const messageToSet = "Hello Swisstronik from feature_earning!!";
+  const messageToSet = "Hello Swisstronik from Happy Cuan Airdrop!!";
   const setMessageTx = await sendShieldedTransaction(signer, contractAddress, contract.interface.encodeFunctionData(functionName, [messageToSet]), 0);
   await setMessageTx.wait();
   console.log("Transaction Receipt: ", setMessageTx);
@@ -143,16 +155,15 @@ main().catch((error) => {
 EOL
 echo "setMessage.js script created."
 
-# Run setMessage.js script
 echo "Running setMessage.js..."
 npx hardhat run scripts/setMessage.js --network swisstronik
 echo "Message set."
 
-# Create getMessage.js script
 echo "Creating getMessage.js script..."
 cat <<EOL > scripts/getMessage.js
 const hre = require("hardhat");
 const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const fs = require("fs");
 
 const sendShieldedQuery = async (provider, destination, data) => {
   const rpclink = hre.network.config.url;
@@ -165,7 +176,7 @@ const sendShieldedQuery = async (provider, destination, data) => {
 };
 
 async function main() {
-  const contractAddress = "0xf84Df872D385997aBc28E3f07A2E3cd707c9698a";
+  const contractAddress = fs.readFileSync("proxiedContract.txt", "utf8").trim();
   const [signer] = await hre.ethers.getSigners();
   const contractFactory = await hre.ethers.getContractFactory("Swisstronik");
   const contract = contractFactory.attach(contractAddress);
@@ -181,9 +192,7 @@ main().catch((error) => {
 EOL
 echo "getMessage.js script created."
 
-# Run getMessage.js script
 echo "Running getMessage.js..."
 npx hardhat run scripts/getMessage.js --network swisstronik
 echo "Message retrieved."
-
-echo "Done! Subscribe: https://t.me/feature_earning"
+echo "Done! Subscribe: https://t.me/GaCryptOfficial"
